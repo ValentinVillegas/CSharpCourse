bf627e524e969f22dc532d8df845fc32421f3da3
ValentinVillegas <45745912+ValentinVillegas@users.noreply.github.com>
2023-06-07 10:47:15 -0600

    Add files via upload

---
+++ EjemploFinally/EjemploFinally/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        StreamReader archivo = null;
+
+        try
+        {
+            string linea;
+            int contador = 0;
+            string path = @"C:\Cursos\CSharp Pildoras\EjemploFinally\ArchivoPrueba.txt";
+
+            archivo = new StreamReader(path);
+
+            while ((linea = archivo.ReadLine()) != null)
+            {
+                Console.WriteLine(linea);
+                contador++;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error con la lectura del archivo");
+        }
+        finally
+        {
+            if (archivo != null) archivo.Close();
+            Console.WriteLine("Se cerró la conexión con el archivo de texto");
+        }
+
+    }
+}
---
+++ EjemplosPOO/EjemplosPOO/Program.cs
@@ -0,0 +1,59 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+        Circulo circulo1 = new Circulo(8);
+        Console.WriteLine($"El área del círculo es de {circulo1.CarlcularArea()} unidades cuadradas.");
+        */
+
+        ConversorEuroDolar objeto = new ConversorEuroDolar();
+        Console.WriteLine($"Con el tipo de cambio a {objeto.ObtenerPrecioDolar()} dolares, 15 euros equivalen a {objeto.ConvertirEnDolares(15)} dolares.");
+        objeto.CambiarPrecioDolar(objeto.ObtenerPrecioDolar() + 0.485);
+        Console.WriteLine($"Con el tipo de cambio a {objeto.ObtenerPrecioDolar()} dolares, 15 euros equivalen a {objeto.ConvertirEnDolares(15)} dolares.");
+
+    }
+}
+
+class Circulo
+{
+    private double PI = 3.1416;
+    private double radio;
+
+    public Circulo(double radio)
+    {
+        this.radio = radio;
+    }
+
+    public double CarlcularArea()
+    {
+        return this.PI * (this.radio * this.radio);
+    }
+}
+
+class ConversorEuroDolar
+{
+    private double precioDolar = 1.253;
+
+    public double ObtenerPrecioDolar()
+    {
+        return this.precioDolar;
+    }
+
+    public void CambiarPrecioDolar(double nuevoPrecio)
+    {
+        if(nuevoPrecio <= 0)
+        {
+            Console.WriteLine("El nuevo precio debe ser mayor a cero");
+        }
+        else
+        {
+            this.precioDolar = nuevoPrecio;
+        }
+    }
+
+    public double ConvertirEnDolares(double cantidadEuros)
+    {
+        return cantidadEuros * this.precioDolar;
+    }
+}
