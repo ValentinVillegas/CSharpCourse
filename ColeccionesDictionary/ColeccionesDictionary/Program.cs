5f53ca3c528d4d7054cd53e727eb788649334b04
ValentinVillegas <45745912+ValentinVillegas@users.noreply.github.com>
2023-06-07 10:53:48 -0600

    Add files via upload

---
+++ ColeccionesDictionary/ColeccionesDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<string,int> personas = new Dictionary<string,int>();
+
+        personas.Add("Juan", 18);
+        personas.Add("Javier", 24);
+        personas["Fernanda"] = 32;
+        personas["Oscar"] = 28;
+
+        foreach (KeyValuePair<string,int> p in personas)
+        {
+            Console.WriteLine($"Nombre: {p.Key} \tEdad: {p.Value}");
+        }
+    }
+}
---
+++ ColeccionesStackPilas/ColeccionesStackPilas/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Stack<int> numeros = new Stack<int>();
+
+        foreach (int num in new int[] {2,4,6,8,10})
+        {
+            //El último elemento agregado a la pila se encuentra al inicio.
+            //El primer elemento agregado a la pila se encuentra al final.
+            numeros.Push(num);
+        }
+
+        numeros.Pop(); //Se elimina el último elemento que se haya agregado a la pila
+
+        foreach (int num in numeros)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
