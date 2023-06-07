aca05c3e4ede7f8bfd834d5087cdd47ddb8e629c
ValentinVillegas <45745912+ValentinVillegas@users.noreply.github.com>
2023-06-07 10:55:59 -0600

    Add files via upload

---
+++ Lambdas/Lambdas/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        int aleatorio;
+        List<int> numeros = new List<int>();
+        string listaNumeros = "";
+        List<int> listaPares = new List<int>();
+
+        for (int i = 0; i<=9; i++)
+        {
+            aleatorio = random.Next(0, 101);
+            numeros.Add(aleatorio);
+        }
+
+        numeros.ForEach(n => listaNumeros += n + ", ");
+        Console.WriteLine($"La lista de números generados aleeatoriamente es la siguiente: {listaNumeros}");
+
+        listaNumeros = "";
+        listaPares = numeros.FindAll(n => n%2 == 0);
+        listaPares.ForEach(n => listaNumeros += n + ", ");
+        Console.WriteLine($"Los números pares en la lista son los siguientes: {listaNumeros}");
+    }
+}
