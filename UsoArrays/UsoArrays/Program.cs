2b23b9f5aa071ec7856b5cc6f4cb8c786ebada4e
ValentinVillegas <45745912+ValentinVillegas@users.noreply.github.com>
2023-06-07 10:48:06 -0600

    Add files via upload

---
+++ UsoArrays/UsoArrays/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numeros = SolicitarNumeros();
+
+        foreach (int i in numeros)
+        {
+            Console.WriteLine(i);
+        }
+
+        /*
+        //int[] edades; //Declaración del Array
+        //edades = new int[4]; //Inicialización del Array
+        int[] edades = { 15, 27, 19, 80 }; //Declaración e inicialización en una sola línea
+        edades[0] = 15; //Asignación al elementon con la posición 0 del Array
+        edades[1] = 27;
+        edades[2] = 19;
+        edades[3] = 80;
+
+        Console.WriteLine(edades[2]);
+
+        var arrEmpleados = new[]
+        {
+            new {Nombre = "Juan", Edad=23},
+            new {Nombre = "María", Edad=28},
+            new {Nombre = "Ana", Edad=26}
+        };
+
+        Console.WriteLine(arrEmpleados[2].Nombre);
+
+        Empleado empleadoAna = new Empleado("Ana", 26);
+
+        Empleado[] empleados = new Empleado[3];
+        empleados[0] = new Empleado("Juan", 23);
+        empleados[1] = new Empleado("María", 28);
+        empleados[2] = empleadoAna;
+
+        Console.WriteLine(empleados[1].getNombre());
+        */
+    }
+
+    static int[] SolicitarNumeros()
+    {
+        Console.Write("¿Cuantos elementos desea ingresar al array?: ");
+        int cantElementos = int.Parse(Console.ReadLine());
+        int[] numeros = new int[cantElementos];
+
+        for (int i=0; i<cantElementos; i++)
+        {
+            Console.Write($"\nIngrese el valor que se guardará en la posición {i}: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        return numeros;
+    }
+}
+
+class Empleado
+{
+    string nombre;
+    int edad;
+
+    public Empleado(string nombre, int edad)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public string getNombre() => nombre;
+}
---
+++ UsoCoches/UsoCoches/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Coche coche1 = new Coche(2300.5, 0.800);
+        coche1.setExtras(true, "Cuero");
+        Console.WriteLine(coche1.GetInfoCoche());
+    }
+}
+
+class Coche
+{
+    private int ruedas;
+    private double largo;
+    private double ancho;
+    private bool climatizador;
+    private string tapiceria;
+
+    public Coche(double largo, double ancho)
+    {
+        this.ruedas = 4;
+        this.largo = largo;
+        this.ancho = ancho;
+        this.tapiceria = "Tela";
+    }
+
+    public void setExtras(bool tieneClima, string tapiceria)
+    {
+        this.climatizador = tieneClima;
+        this.tapiceria=tapiceria;
+    }
+
+    public string getExtras()
+    {
+        return $"====== Extras del coche: ======" +
+            $"\nTiene aire acondicionado: {climatizador}" +
+            $"\nTapiceria: {this.tapiceria}";
+    }
+
+    public string GetInfoCoche()
+    {
+        return $"====== Información del coche: ======" +
+            $"\nCantidad de ruedas: {this.ruedas}" +
+            $"\nLargo: {this.largo}" +
+            $"\nAncho: {this.ancho}" + "\n" + getExtras();
+    }
+}
