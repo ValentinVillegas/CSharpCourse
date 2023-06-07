e4b4161fee736b807a4f465c09b9646308bba66a
ValentinVillegas <45745912+ValentinVillegas@users.noreply.github.com>
2023-06-07 10:42:18 -0600

    Área círculo

---
+++ AreaCirculo/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ingrese el valor del radio del circulo: ");
+
+        double radio = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"El circulo tiene un radio de {radio} unidades.");
+        Console.WriteLine($"El circulo tiene un área de {Math.PI * Math.Pow(radio, 2)} unidades cuadradas.");
+    }
+}
