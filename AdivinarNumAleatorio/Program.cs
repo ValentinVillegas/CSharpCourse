b2c74eb440dee6ea39de2503b212fca841d84ae9
ValentinVillegas <45745912+ValentinVillegas@users.noreply.github.com>
2023-06-07 10:46:14 -0600

    Add files via upload

---
+++ AdivinarNumAleatorio/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random aleatorio = new Random();
+        int numeroAleatorio = aleatorio.Next(0, 101);
+
+        int numeroIngresado = -1;
+        int intentosRealizados = 0;
+        bool numeroValido = false;
+
+        Console.Write("Ingrese un número entre 0 y 100: ");
+
+        do
+        {
+            while (!numeroValido)
+            {
+                try
+                {
+                    numeroIngresado = int.Parse(Console.ReadLine());
+                    if (numeroIngresado < numeroAleatorio) Console.Write("Ingresa un número más alto: ");
+                    if (numeroIngresado > numeroAleatorio) Console.Write("Ingresa un número más bajo: ");
+                    numeroValido = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("El número ingresado no es un número válido");
+                    Console.Write("Ingresa un número: ");
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("El número intriducido es demasiado alto.");
+                    Console.Write("Ingresa un número: ");
+                }
+            }
+
+            intentosRealizados++;
+            numeroValido = false;
+        } while (numeroIngresado != numeroAleatorio);
+
+        Console.WriteLine($"\n!Haz acertado el número aleatorio en {intentosRealizados} intentos¡.");
+    }
+}
---
+++ LanzamientoExcepciones/LanzamientoExcepciones/Program.cs
@@ -0,0 +1,63 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ingresa un número de mes: ");
+        int mesIngresado = int.Parse(Console.ReadLine());
+
+        try
+        {
+            Console.WriteLine($"El nombre para el mes ingresado es: {nombreDelMes(mesIngresado)}");
+        }catch(Exception ex)
+        {
+            Console.WriteLine($"Mensaje de la excepción producida: {ex.Message}");
+        }
+
+        Console.WriteLine("Aquí continuaría la ejecución del programa");
+    }
+
+    public static string nombreDelMes(int mes)
+    {
+        switch (mes)
+        {
+            case 1:
+                return "Enero";
+
+            case 2:
+                return "Febrero";
+
+            case 3:
+                return "Marzo";
+
+            case 4:
+                return "Abril";
+
+            case 5:
+                return "Mayo";
+
+            case 6:
+                return "Junio";
+
+            case 7:
+                return "Julio";
+
+            case 8:
+                return "Agosto";
+            
+            case 9:
+                return "Septiembre";
+
+            case 10:
+                return "Octubre";
+
+            case 11:
+                return "Noviembre";
+
+            case 12:
+                return "Diciembre";
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
