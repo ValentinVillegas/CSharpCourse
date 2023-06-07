76570ccc99e662dde870e2e87266e43d80122b14
ValentinVillegas <45745912+ValentinVillegas@users.noreply.github.com>
2023-06-07 10:45:23 -0600

    Add files via upload

---
+++ SobrecargaYParametrosOpcionales/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ingrese el valor del primer número: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el valor del segundo número: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el valor del tercer número: ");
+        double numero3 = double.Parse(Console.ReadLine());
+
+        //Llamada al método Suma con 2 parámetros
+        Console.WriteLine($"La suma de {numero1} + {numero2} es de {Suma(numero1,numero2)}");
+
+        //Llamada al método Suma con 2 parámetros de diferente tipo
+        Console.WriteLine($"La suma de {numero1} + {numero3} es de {Suma(numero1, numero3)}");
+
+        //Llamada al método Suma con 3 parámetros, 1 de ellos es opcional
+        Console.WriteLine($"La suma de {numero1} + {numero2} es de {Suma(numero1, numero2)}");
+    }
+
+    private static int Suma(int numero1, int numero2)
+    {
+        return numero1 + numero2;
+    }
+
+    private static double Suma(int numero1, double numero2)
+    {
+        return numero1 + numero2;
+    }
+
+    private static double Suma(int numero1, int numero2, int numero3 = 0)
+    {
+        return numero1 + numero2 + numero3;
+    }
+}
---
+++ SumaDosNumeros/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero1, numero2;
+
+Console.Write("Ingrese el valor del primer número: ");
+numero1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el valor del segundo número: ");
+numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"La suma de {numero1} + {numero2} es de {numero1 + numero2}");
