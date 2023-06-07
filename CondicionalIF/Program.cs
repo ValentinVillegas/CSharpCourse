3451ceafd1ba39de96cf8af2f6c45e6fc7e11e50
ValentinVillegas <45745912+ValentinVillegas@users.noreply.github.com>
2023-06-07 10:44:25 -0600

    Add files via upload

---
+++ CondicionalIF/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("PROGRAMA PARA DETERMINAR SI PUEDES CONDUCIR VEHICULOS");
+
+        Console.Write("Ingresa tu edad: ");
+
+        int edad = int.Parse(Console.ReadLine());
+        bool tieneLicnencia = false;
+
+        if(edad < 18)
+        {
+            Console.WriteLine("No puedes conducir vehículos");
+        }
+        else
+        {
+            Console.Write("¿Cuentas con licencia para conducir? 1:Si  2:No :");
+
+            tieneLicnencia = int.Parse(Console.ReadLine()) == 1;
+
+            if (!tieneLicnencia)
+            {
+                Console.WriteLine("No puedes conducir vehiculos");
+            }
+            else
+            {
+                Console.WriteLine("¡Adelante! Puedes conducir vehiculos");
+            }
+        }
+    }
+}
---
+++ PrimeraApp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenida al curso de C#!");
+
+
+
+        //Interpolación de strings
+        int edad = 23;
+        edad++; //Se incrementa en uno el valor de la variable y este nuevo valor se asigna a la misma variable edad
+        Console.WriteLine($"Tienes una edad de {edad} años");
+    }
+}
